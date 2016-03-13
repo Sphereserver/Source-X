d8eebc6f94693ab8a36bc40b3a9c2f0da11df75a
nefthon <fjgo1986@gmail.com>
2016-03-13 11:59:03 +0100

    Moved back templates from cpp to .h

--- src/sphere/containers.h
+++ src/sphere/containers.h
@@ -20,30 +20,87 @@ private:
 	iterator m_tail;
 
 public:
-	ThreadSafeQueue();
+	ThreadSafeQueue()
+	{
+		m_list.push_back( T() ); // at least one element must be in the queue
+		m_head = m_list.begin();
+		m_tail = m_list.end();
+	}
 
 private:
-	ThreadSafeQueue(const ThreadSafeQueue& copy);
-	ThreadSafeQueue& operator=(const ThreadSafeQueue& other);
+	ThreadSafeQueue( const ThreadSafeQueue& copy );
+	ThreadSafeQueue& operator=( const ThreadSafeQueue& other );
 
 public:
 	// Append an element to the end of the queue (writer)
-	void push(const T& value);
+	void push( const T& value )
+	{
+		m_list.push_back( value );
+		m_tail = m_list.end();
+		clean();
+	}
 
 	// Erase elements from before reader head (writer)
-	void clean(void);
+	void clean( void )
+	{
+		m_list.erase( m_list.begin(), m_head );
+	}
 
 	// Retrieve the number of elements in the queue (reader/writer)
-	size_t size(void) const;
+	size_t size( void ) const
+	{
+		if ( empty() )
+			return 0;
+
+		size_t toSkip = 1;
+		for ( const_iterator it = m_list.begin(); it != m_head && it != m_list.end(); ++it )
+		{
+			if ( it == m_list.end() )
+				break;
+
+			toSkip++;
+		}
+
+		return m_list.size() - toSkip;
+	}
 
 	// Determine if the queue is empty (reader/writer)
-	bool empty(void) const;
+	bool empty( void ) const
+	{
+		iterator next = m_head;
+		++next;
+
+		return ( next == m_tail );
+	}
 
 	// Remove the first element from the queue (reader)
-	void pop(void);
+	void pop( void )
+	{
+		if ( empty() )
+			throw CException( LOGL_ERROR, 0, "No elements to read from queue." );
+
+		iterator next = m_head;
+		++next;
+
+		if ( next != m_tail )
+			m_head = next;
+	}
 
 	// Retrieve the first element in the queue (reader)
-	T front(void) const;
+	T front( void ) const
+	{
+		if ( empty() == false )
+		{
+			iterator next = m_head;
+			++next;
+
+			if ( next != m_tail )
+				return *next;
+		}
+
+		// this should never happen
+		throw CException( LOGL_ERROR, 0, "No elements to read from queue." );
+	}
 };
 
-#endif // _INC_CONTAINERS_H_
+#endif
