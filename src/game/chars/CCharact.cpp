a9e6e8253ae7e862663744db1946aaed802a8095
drk84 <pietro.piscioneri@gmail.com>
2017-10-28 13:57:30 +0200

    Fix from Source1 made by Coruja, addressing the following problem: Hidden chars are not shown when being stumbled
    See:
    https://github.com/Sphereserver/Source/commit/9b58fb8076d1efd94b4e6babf9e8c9bec2ad4900
    And:
    https://github.com/Sphereserver/Source/issues/84

--- src/game/chars/CCharact.cpp
+++ src/game/chars/CCharact.cpp
@@ -2973,7 +2973,7 @@ CRegionBase * CChar::CanMoveWalkTo( CPointBase & ptDst, bool fCheckChars, bool f
 			return NULL;
 		}
 
-		if ( Stat_GetVal(STAT_DEX) <= 0 && !IsStatFlag(STATF_DEAD) )
+		if ( (Stat_GetVal(STAT_DEX) <= 0) && (!IsStatFlag(STATF_DEAD)) )
 		{
 			SysMessageDefault(DEFMSG_MSG_FATIGUE);
 			return NULL;
@@ -3021,14 +3021,14 @@ CRegionBase * CChar::CanMoveWalkTo( CPointBase & ptDst, bool fCheckChars, bool f
 	short iStamReq = 0;
 	if ( fCheckChars && !IsStatFlag(STATF_DEAD|STATF_Sleeping|STATF_Insubstantial) )
 	{
-		CItem * pPoly = LayerFind(LAYER_SPELL_Polymorph);
-		CWorldSearch AreaChars( ptDst );
+		CItem *pPoly = LayerFind(LAYER_SPELL_Polymorph);
+		CWorldSearch AreaChars(ptDst);
 		for (;;)
 		{
-			CChar * pChar = AreaChars.GetChar();
-			if ( pChar == NULL )
+			CChar *pChar = AreaChars.GetChar();
+			if (!pChar )
 				break;
-			if ( pChar == this || abs(pChar->GetTopZ() - ptDst.m_z) > 5 || pChar->IsStatFlag(STATF_Insubstantial) )
+			if ( (pChar == this) || (abs(pChar->GetTopZ() - ptDst.m_z) > 5) || (pChar->IsStatFlag(STATF_Insubstantial)) )
 				continue;
 			if ( m_pNPC && pChar->m_pNPC )	// NPCs can't walk over another NPC
 				return NULL;
@@ -3036,7 +3036,7 @@ CRegionBase * CChar::CanMoveWalkTo( CPointBase & ptDst, bool fCheckChars, bool f
 			iStamReq = 10;
 			if ( IsPriv(PRIV_GM) || pChar->IsStatFlag(STATF_DEAD|STATF_Invisible|STATF_Hidden) )
 				iStamReq = 0;
-			else if ( pPoly && pPoly->m_itSpell.m_spell == SPELL_Wraith_Form && GetTopMap() == 0 )		// chars under Wraith Form effect can always walk through chars in Felucca
+			else if ( (pPoly && pPoly->m_itSpell.m_spell == SPELL_Wraith_Form) && (GetTopMap() == 0) )		// chars under Wraith Form effect can always walk through chars in Felucca
 				iStamReq = 0;
 
 			TRIGRET_TYPE iRet = TRIGRET_RET_DEFAULT;
@@ -3048,11 +3048,12 @@ CRegionBase * CChar::CanMoveWalkTo( CPointBase & ptDst, bool fCheckChars, bool f
 
 				if ( iRet == TRIGRET_RET_TRUE )
 					return NULL;
+				if (iStamReq < 0)
+					continue;
 			}
 
-			if ( iStamReq <= 0 )
-				continue;
-			if ( Stat_GetVal(STAT_DEX) < Stat_GetMax(STAT_DEX) )
+			
+			if ( (iStamReq > 0) && (Stat_GetVal(STAT_DEX) < Stat_GetMax(STAT_DEX)) )
 				return NULL;
 
 			tchar *pszMsg = Str_GetTemp();
@@ -3083,7 +3084,7 @@ CRegionBase * CChar::CanMoveWalkTo( CPointBase & ptDst, bool fCheckChars, bool f
 	{
 		EXC_SET("Stamina penalty");
 		// Chance to drop more stamina if running or overloaded
-		CVarDefCont * pVal = GetKey("OVERRIDE.RUNNINGPENALTY", true);
+		CVarDefCont *pVal = GetKey("OVERRIDE.RUNNINGPENALTY", true);
 		if ( IsStatFlag(STATF_Fly|STATF_Hovering) )
 			iWeightLoadPercent += pVal ? (int)(pVal->GetValNum()) : g_Cfg.m_iStamRunningPenalty;
 
