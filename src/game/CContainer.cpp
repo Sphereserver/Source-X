b94b3fc03b6793712ce5b27128ec361a325c3a89
cbnolok <cbnolok@gmail.com>
2020-04-07 19:08:39 +0200

    Update CContainer.cpp

--- src/game/CContainer.cpp
+++ src/game/CContainer.cpp
@@ -394,18 +394,6 @@ void CContainer::ContentsDump( const CPointMap &pt, uint64 iAttrLeave )
 	// Just dump the contents onto the ground.
 
     iAttrLeave |= ATTR_NEWBIE|ATTR_MOVE_NEVER|ATTR_CURSED2|ATTR_BLESSED2;
-    /*
-	for (size_t i = 0; i < GetContentCount(); )
-	{
-		CItem* pItem = static_cast<CItem*>(GetContentIndex(i));
-		if ( pItem->IsAttr(iAttrLeave) )	// hair and newbie stuff.
-			continue;
-
-		// ??? scatter a little ?
-		if (!pItem->MoveToCheck(pt))
-			++i;
-	}
-	*/
 	
 	for (size_t i = 0; i < GetContentCount(); )
 	{
