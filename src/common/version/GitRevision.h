831288461f8c20833d65cb22cc0a2c34294902ea
xwerswoodx <hamitdevecioglu@gmail.com>
2020-12-16 16:08:58 +0000

    Fixed unlockitem -1 not remove attribute flag from items inside multi.

--- src/common/version/GitRevision.h
+++ src/common/version/GitRevision.h
@@ -1,2 +1,2 @@
-#define __GITHASH__ "d9e3097e7ac77b8b0a2cec71a360fb656409b281"
-#define __GITREVISION__ 3291
+#define __GITHASH__ "cde56bb0516f1c6b879defd94a9a23b42c833967"
+#define __GITREVISION__ 3344
