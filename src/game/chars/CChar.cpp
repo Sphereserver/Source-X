66fed5f1faffefd90e66dd0170326468d1ac386a
Tolokio <ldroman@outlook.es>
2022-12-10 13:35:25 +0100

    Update CChar.cpp

--- src/game/chars/CChar.cpp
+++ src/game/chars/CChar.cpp
@@ -156,8 +156,9 @@ lpctstr const CChar::sm_szTrigName[CTRIG_QTY+1] =	// static
 	"@PartyLeave",
 	"@PartyRemove",			//I have ben removed from the party by SRC
 
-    "@PayGold",             // I'm going to give out money for a service (Skill Training, hiring...).
-	"@PersonalSpace",		// +i just got stepped on.
+	"@PayGold",             // I'm going to give out money for a service (Skill Training, hiring...).
+	"@PersonalSpace",		// +i just got stepped on by other char.
+	"@PersonalSpace2",		// +i am stepping on other char.
 	"@PetDesert",			// I just went wild again
 	"@Profile",				// someone hit the profile button for me.
 	"@ReceiveItem",			// I was just handed an item (Not yet checked if i want it)
