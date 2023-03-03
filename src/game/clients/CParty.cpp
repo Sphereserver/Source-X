1f891d5bed9f1ac2c35b8990c4febf0d9d500727
David Kindl <david.kindl@aira.cz>
2023-03-03 11:14:33 +0100

    - cast "" to CSString, since sVal is already CSString (fixes error C2445 in C++20)

--- src/game/clients/CParty.cpp
+++ src/game/clients/CParty.cpp
@@ -641,7 +641,7 @@ bool CPartyDef::r_WriteVal( lpctstr ptcKey, CSString &sVal, CTextConsole *pSrc, 
 			break;
 
 		case PDC_SPEECHFILTER:
-			sVal = this->m_pSpeechFunction.IsEmpty() ? "" : this->m_pSpeechFunction;
+			sVal = this->m_pSpeechFunction.IsEmpty() ? (CSString) "" : this->m_pSpeechFunction;
 			break;
 
 		case PDC_TAG0:
