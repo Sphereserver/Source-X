b429465abcd325cf8aa6535315fbe3f83c218fdb
Tolokio <ldroman@outlook.es>
2023-08-17 16:24:32 +0200

    POISON POTION fix Update CClientUse.cpp

--- src/game/clients/CClientUse.cpp
+++ src/game/clients/CClientUse.cpp
@@ -280,11 +280,21 @@ bool CClient::Cmd_Use_Item( CItem *pItem, bool fTestTouch, bool fScript )
 			}
 			if ( RES_GET_INDEX(pItem->m_itPotion.m_Type) == SPELL_Poison )
 			{
-				// If we click directly on poison potion, we will drink poison and get ill.
-				// To use it on Poisoning skill, the skill will request to target the potion.
-				m_pChar->OnSpellEffect(SPELL_Poison, m_pChar, pItem->m_itSpell.m_spelllevel, nullptr);
-				return true;
-			}
+					// If we click directly on poison potion, we will drink poison and get ill.
+					// To use it on Poisoning skill, the skill will request to target the potion.
+					// 
+					//Let sphere find the empty "bottle"(maybe someone script a poison on a pitcher)
+					const CItemBase* pItemDef = pItem->Item_GetDef();
+					ITEMID_TYPE idbottle = (ITEMID_TYPE)pItemDef->m_ttDrink.m_ridEmpty.GetResIndex();
+					m_pChar->OnSpellEffect(SPELL_Poison, m_pChar, pItem->m_itSpell.m_spelllevel, pItem );
+					//Sound(sm_DrinkSounds[Calc_GetRandVal(CountOf(sm_DrinkSounds))]);
+					m_pChar->UpdateAnimate(ANIM_EAT);
+					pItem->ConsumeAmount();
+					// Create the empty bottle ?
+					if (idbottle != ITEMID_NOTHING)
+						m_pChar->ItemBounce(CItem::CreateScript(idbottle, m_pChar), false);
+					return true;
+				}
 			if ( RES_GET_INDEX(pItem->m_itPotion.m_Type) == SPELL_Explosion )
 			{
 				// Throw explosion potion
