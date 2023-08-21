a6405fe4969052e14ad646f6e941ef0d08eee820
Tolokio <ldroman@outlook.es>
2023-08-21 20:50:22 +0200

    Update CClientUse.cpp

--- src/game/clients/CClientUse.cpp
+++ src/game/clients/CClientUse.cpp
@@ -278,13 +278,6 @@ bool CClient::Cmd_Use_Item( CItem *pItem, bool fTestTouch, bool fScript )
 				SysMessageDefault(DEFMSG_ITEMUSE_POTION_FAIL);
 				return false;
 			}
-			if ( RES_GET_INDEX(pItem->m_itPotion.m_Type) == SPELL_Poison )
-			{
-				// If we click directly on poison potion, we will drink poison and get ill.
-				// To use it on Poisoning skill, the skill will request to target the potion.
-				m_pChar->OnSpellEffect(SPELL_Poison, m_pChar, pItem->m_itSpell.m_spelllevel, nullptr);
-				return true;
-			}
 			if ( RES_GET_INDEX(pItem->m_itPotion.m_Type) == SPELL_Explosion )
 			{
 				// Throw explosion potion
