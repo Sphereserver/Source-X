ea27639fa558813f895e22f2bacccd638044e946
cbnolok <claudiobng@gmail.com>
2023-09-16 13:26:28 +0200

    Fixed LibEV configuration generation CMake script.

--- cmake/CMakeConditionalConfigureLibev.cmake
+++ cmake/CMakeConditionalConfigureLibev.cmake
@@ -1,14 +1,32 @@
 # Generate config.h file for libev (if we are using it)
-SET (USE_EV 0)
+
+FUNCTION (contains_compile_definition  definition ret)
+SET (result FALSE PARENT_SCOPE)
+
 FOREACH (TARG ${TARGETS})
 	GET_TARGET_PROPERTY (DEFS ${TARG} COMPILE_DEFINITIONS)
 	FOREACH (DEF ${DEFS})
-		IF ("${DEF}" STREQUAL "_LIBEV")
-			SET (USE_EV 1)
-		ENDIF ("${DEF}" STREQUAL "_LIBEV")
-	ENDFOREACH (DEF ${DEFS})
-ENDFOREACH (TARG ${TARGETS})
+		IF ("${DEF}" STREQUAL "${definition}")
+			SET (ret TRUE PARENT_SCOPE)
+			RETURN ()
+		ENDIF ()
+	ENDFOREACH ()
+ENDFOREACH ()
 
-IF (USE_EV)
+GET_DIRECTORY_PROPERTY( DEFS COMPILE_DEFINITIONS )
+FOREACH (DEF ${DEFS})
+	IF ("${DEF}" STREQUAL "${definition}")
+		SET (ret TRUE PARENT_SCOPE)
+		RETURN ()
+	ENDIF ()
+ENDFOREACH ()
+
+ENDFUNCTION ()
+
+
+SET (ret FALSE)
+contains_compile_definition("_LIBEV" ret)
+
+IF (ret)
 	INCLUDE ("lib/libev/cmake/configure.cmake")
-ENDIF (USE_EV)
+ENDIF (ret)
