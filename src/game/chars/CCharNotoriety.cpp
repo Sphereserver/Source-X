6298e0eb648224a0d0f5ac07d9ee8554b2eadf91
Gladie <gladie38@gmail.com>
2024-02-03 22:29:49 +0100

    Update CCharNotoriety.cpp

--- src/game/chars/CCharNotoriety.cpp
+++ src/game/chars/CCharNotoriety.cpp
@@ -225,12 +225,8 @@ NOTO_TYPE CChar::Noto_CalcFlag(const CChar * pCharViewer, bool fAllowIncog, bool
 									{
 										return NOTO_GUILD_WAR;
 									}
-										
-									return NOTO_GUILD_WAR;
-
 								}
 							}
-
 							if (pViewerGuild == pMyGuild) // Same guild?
 								return NOTO_GUILD_SAME; // return green
 							if (pMyGuild->IsAlliedWith(pViewerGuild))
