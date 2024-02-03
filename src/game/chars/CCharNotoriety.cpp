0210db524346a9a4d97671e5524c76b97c6bb3f6
Gladie <gladie38@gmail.com>
2024-02-03 21:36:10 +0100

    Extension to guild notoriety
    Add option to change notoriety for guild with same ALIGN
    Fix for O vs CH Orange notoriety

--- src/game/chars/CCharNotoriety.cpp
+++ src/game/chars/CCharNotoriety.cpp
@@ -214,13 +214,17 @@ NOTO_TYPE CChar::Noto_CalcFlag(const CChar * pCharViewer, bool fAllowIncog, bool
 							{
 								if (pViewerGuild->GetAlignType() != STONEALIGN_STANDARD)
 								{
-									if (pViewerGuild->GetAlignType() == pMyGuild->GetAlignType())
+									if (IsSetOF(OF_EnableSameAlignGreenNotoriety))
 									{
-										return NOTO_GUILD_SAME;
+										if (pViewerGuild->GetAlignType() == pMyGuild->GetAlignType())
+										{
+											return NOTO_GUILD_SAME;
+										}		
+									}		
+									if (pViewerGuild->GetAlignType() == STONEALIGN_ORDER) && (pMyGuild->GetAlignType() == STONEALIGN_CHAOS) || (pViewerGuild->GetAlignType() == STONEALIGN_CHAOS) && (pMyGuild->GetAlignType() == STONEALIGN_ORDER)
+									{
+										return NOTO_GUILD_WAR;
 									}
-										
-									return NOTO_GUILD_WAR;
-
 								}
 							}
 
