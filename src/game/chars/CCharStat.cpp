6e43a524747573e095e8c9ca115709d6dbd8da8a
Tolokio <ldroman@outlook.es>
2024-06-30 19:46:28 +0200

    Update CCharStat.cpp
    removed problematic stuff.

--- src/game/chars/CCharStat.cpp
+++ src/game/chars/CCharStat.cpp
@@ -334,17 +334,6 @@ void CChar::Stat_SetBase( STAT_TYPE i, ushort uiVal )
 				return;
 			// do not restore argn1 to i, bad things will happen! leave i untouched. (matex)
 			uiVal = (ushort)(args.m_iN3);
-
-			if (i != STAT_FOOD && m_Stat[i].m_max < 1) // MaxFood cannot depend on something, otherwise if the Stat depends on STR, INT, DEX, fire MaxHits, MaxMana, MaxStam
-			{
-				args.m_iN1 = i + 4LL; // Shift by 4 to indicate MaxHits, MaxMana, MaxStam
-				args.m_iN2 = uiStatVal;
-				args.m_iN3 = uiVal;
-				if (OnTrigger(CTRIG_StatChange, this, &args) == TRIGRET_RET_TRUE)
-					return;
-				// do not restore argn1 to i, bad things will happen! leave i untouched. (matex)
-				uiVal = (ushort)(args.m_iN3);
-			}
 		}
 	}
 	switch ( i )
