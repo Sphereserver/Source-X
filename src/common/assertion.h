89e27bfd3e578e67b5c8973890e9c5d952edc977
cbnolok <cbnolo@gmail.com>
2024-07-20 14:06:23 +0200

    Added error message when trying to set a stat to an invalid value.

--- src/common/assertion.h
+++ src/common/assertion.h
@@ -4,18 +4,21 @@
 
 extern void Assert_Fail(const char * pExp, const char *pFile, long long llLine);
 
-#define PERSISTANT_ASSERT(exp)	if ( !(exp) )	Assert_Fail(#exp, __FILE__, __LINE__)
+#define PERSISTANT_ASSERT(exp)  if ( !(exp) )   Assert_Fail(#exp, __FILE__, __LINE__)
 
-#if defined(_NIGHTLY) || defined(_DEBUG) || defined(__COVERITY__)
-    #define ASSERT(exp)		    if ( !(exp) )	Assert_Fail(#exp, __FILE__, __LINE__)
+#if defined(__COVERITY__)
+    #include <cassert>
+    #define ASSERT(exp)         assert(exp)
+#elif defined(_NIGHTLY) || defined(_DEBUG)
+    #define ASSERT(exp)         if ( !(exp) )   Assert_Fail(#exp, __FILE__, __LINE__)
 #else
-    #define ASSERT(exp)			(void)0
+    #define ASSERT(exp)         (void)0
 #endif
 
 #if defined(_DEBUG) || defined(__COVERITY__)
-    #define DEBUG_ASSERT(exp)		    if ( !(exp) )	Assert_Fail(#exp, __FILE__, __LINE__)
+    #define DEBUG_ASSERT(exp)   if ( !(exp) )   Assert_Fail(#exp, __FILE__, __LINE__)
 #else
-    #define DEBUG_ASSERT(exp)			(void)0
+    #define DEBUG_ASSERT(exp)   (void)0
 #endif
 
 
