01284202dcf7860538a614a77e1882ffeecc62a2
cbnolok <cbnolok@gmail.com>
2025-02-03 17:37:57 +0100

    More CWorldTicker cleanup, removed some redundant tick list add ops.

--- src/game/CWorldTicker.h
+++ src/game/CWorldTicker.h
@@ -8,42 +8,6 @@
 #include "CTimedFunctionHandler.h"
 #include "CTimedObject.h"
 
-/*
- * #include <flat_containers/flat_set.hpp>
-*/
-
-/*
-//--- Include phmap.h
-#ifdef ADDRESS_SANITIZER
-    #define MYASAN_
-#endif
-
-#ifdef _WIN32
-//    #define MYSRWLOCK_
-    #undef SRWLOCK_INIT
-#endif
-#ifdef __GNUC__
-    #pragma GCC diagnostic push
-    #pragma GCC diagnostic ignored "-Wshift-count-overflow"
-#endif
-
-// TODO: undef is TEMPORARY !! There's a bug that needs to be solved
-#undef ADDRESS_SANITIZER
-#include <parallel_hashmap/phmap.h>
-
-#ifdef MYASAN_
-    #define ADDRESS_SANITIZER
-#endif
-//#ifdef MYSRWLOCK_
-//#   define SRWLOCK_INIT
-//#endif
-
-#ifdef __GNUC__
-    #pragma GCC diagnostic pop
-#endif
-//--- End of phmap.h inclusion
-*/
-
 
 class CObjBase;
 class CChar;
@@ -72,9 +36,6 @@ private:
     };
 
     // Calls to OnTickStatusUpdate. Periodically send updated infos to the clients.
-    //struct StatusUpdatesList : public phmap::parallel_flat_hash_set<CObjBase*>
-    //struct StatusUpdatesList : public fc::flat_set<CObjBase*>
-    //struct StatusUpdatesList : public std::unordered_set<CObjBase*>
     struct StatusUpdatesList : public std::vector<CObjBase*>
     {
         MT_CMUTEX_DEF;
