68edeebacecc8a214b5e7a3253d79469ced8a35a
David Kindl <david.kindl@aira.cz>
2026-03-04 10:58:42 +0100

    Revert "Optional flag to bypass heal interrupt on hit"
    This reverts commit fc9472912344c1ea8a53127bb2697463715c5dbb.
    
    Removed heal interrupt completely with info, how to add it to scripts

--- src/game/chars/CCharFight.cpp
+++ src/game/chars/CCharFight.cpp
@@ -2200,13 +2200,6 @@ WAR_SWING_TYPE CChar::Fight_Hit( CChar * pCharTarg )
         }
 	}
 
-	// BAD BAD Healing fix.. Cant think of something else -- Radiant
-	if (pCharTarg->m_Act_SkillCurrent == SKILL_HEALING && !IsSetOF(OF_DontInterruptHealOnHit))
-	{
-		pCharTarg->SysMessageDefault(DEFMSG_HEALING_INTERRUPT);
-		pCharTarg->Skill_Cleanup();
-	}
-
 	if ( pAmmo )
 	{
 		if ( pCharTarg->m_pNPC && (40 >= g_Rand.Get16ValFast(100)) )
