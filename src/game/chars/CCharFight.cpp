df70547437dedbc2691e6aeb6666217b4a3f5e70
David Kindl <david.kindl@aira.cz>
2026-03-12 17:01:18 +0100

    Merge fix

--- src/game/chars/CCharFight.cpp
+++ src/game/chars/CCharFight.cpp
@@ -1376,8 +1376,6 @@ bool CChar::Fight_Clear(CChar *pChar, bool fForced)
         m_atFight.m_iSwingAnimationDelay = 0;
         m_atFight.m_iSwingAnimation = 0;
         m_atFight.m_iSwingIgnoreLastHitTag = 0;
-	{
-        m_atFight.m_iWarSwingState = WAR_SWING_EQUIPPING;
 		m_Fight_Targ_UID.InitUID();
     }
 
